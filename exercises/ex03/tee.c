c33a8fa04752d9f2f5f8c39f31e5ef0ec6e10874
SeunginLyu <seungin.lyu@students.olin.edu>
2019-02-20 21:12:02 -0500

    Add multiple file argv

--- exercises/ex03/tee.c
+++ exercises/ex03/tee.c
@@ -4,12 +4,14 @@
 #include <unistd.h>
 
 #define MAX_FILENAME 255
+#define MAX_FILECOUNT 1000
 int A_FLAG = 0;
 int P_FLAG = 0;
 
 int main(int argc, char *argv[]){
     char ch;
-    char f[MAX_FILENAME];
+    char f[MAX_FILECOUNT][MAX_FILENAME];
+    int file_count = 0;
     // parse arguments
     while((ch = getopt(argc, argv, "ap")) != EOF){
         switch (ch){
@@ -26,20 +28,20 @@ int main(int argc, char *argv[]){
     }
     argc -=optind;
     argv +=optind;
-
-    // if no file name supplied or more than one supplied
-    if(argv[0]==NULL || argv[1] != NULL){
-        fprintf(stderr, "Error: Exactly one file name required\n");
+    if(argv[0] == NULL){
+        fprintf(stderr, "Error: At least one file required\n");
         return 1;
     }
-    // if file name exceeds 255 characters
-    if(strlen(argv[0]) > 255){
-        fprintf(stderr, "Error: The max character number for file names is 255");
-        return 1;
+    while(argv[file_count] != NULL && file_count < MAX_FILECOUNT){
+        // if file name exceeds 255 characters
+        if(strlen(argv[file_count]) > 255){
+            fprintf(stderr, "Error: The max character number for file names is 255");
+            return 1;
+        }
+        else{
+            strcpy(f[file_count],argv[file_count]);
+        }
+        file_count++;
     }
-    else{
-        strcpy(f,argv[0]);
-    }
-    
     return 0;
 }
